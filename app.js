016417adb1296bfd4199137a3fa7cd7eb472eba0
Serhii <xx.code4u@gmail.com>
2024-02-28 14:04:07 +0200

    troubles witch auth

--- app.js
+++ app.js
@@ -1,19 +1,28 @@
+const nodemon = require("nodemon");
+const fs = require("fs");
+
+const colors = require("colors/safe");
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.use((req, res, next) => {
+  console.log("Наше проміжне ПЗ");
+  next();
 });
 
-app.listen(3000, () => {
-  console.log("Example app listening on port 3000!");
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
 app.get("/contact", (req, res) => {
   res.send("<h1>Contact page</h1>");
 });
 
-app.use((req, res, next) => {
-  console.log("Наше проміжне ПЗ");
-  next();
+app.get("/contact/:id", (req, res) => {
+  res.send(`<h1>Contact</h1> Параметр: ${req.params.id}`);
+});
+
+let port = 3001;
+app.listen(port, () => {
+  console.log(`Сервак їбашить на: http://localhost:${port}`);
 });
